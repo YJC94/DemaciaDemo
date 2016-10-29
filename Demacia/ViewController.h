458b80d2709272533cd3c8e1057a78734d06f2fa
Leo <devtip@163.com>
2016-10-29 21:52:04 +0800

    初始化项目

---
+++ Demacia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demacia
+//
+//  Created by 苑金仓 on 2016/10/29.
+//  Copyright © 2016年 PrPrLive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demacia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demacia
+//
+//  Created by 苑金仓 on 2016/10/29.
+//  Copyright © 2016年 PrPrLive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
